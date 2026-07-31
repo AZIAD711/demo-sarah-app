5a6b9ac18a5f1cf912cc1b30ec26e53768062ec9
zeyad <aziad711@gmail.com>
2026-07-31 23:29:51 +0300

    ✅ NEW VALDIATION SCHEMA ADDED : LOGIN & SIGN-UP & RESET PASSWORD

--- src/module/auth/auth.routing.js
+++ src/module/auth/auth.routing.js
@@ -1,12 +1,14 @@
 import {signupController,loginController,getProfileController,updateProfileController, getAllProfileController,forgetPasswordController, resetPasswordController, setStatusAccountController, deleteAccountController} from "./auth.controller.js"
 import {authencation, authorization} from "../../common/middleware/auth.middelware.js"
+import {schemaValidate} from "../../common/middleware/valdiate.js"
+import {loginSchema,resetPasswordSchema,signupSchema} from "./auth.valdiation.js"
 import express from "express"
 import { Role } from "../../common/enum/role.js"
 const userRouter = express.Router()
-userRouter.post("/signup",signupController)
-userRouter.post("/login",loginController)
+userRouter.post("/signup",schemaValidate(signupSchema),signupController)
+userRouter.post("/login",schemaValidate(loginSchema),loginController)
 userRouter.post("/forget/password",forgetPasswordController)
-userRouter.put("/reset/password",resetPasswordController)
+userRouter.put("/reset/password",schemaValidate(resetPasswordSchema),resetPasswordController)
 userRouter.get("/profile",authencation(),authorization(Role.USER,Role.ADMIN),getProfileController)
 userRouter.get("/profile/all",authencation(),authorization(Role.ADMIN),getAllProfileController)
 userRouter.put("/profile/update",authencation(),authorization(Role.USER,Role.ADMIN),updateProfileController)
---
+++ src/module/auth/auth.valdiation.js
@@ -0,0 +1,107 @@
+import Joi from "joi";
+import { Gender } from "../../common/enum/gender.js"
+import { Role } from "../../common/enum/role.js"
+// LOGIN SCHEMA 
+export const loginSchema = {
+    body: Joi.object({
+        // EMAIL
+        email: Joi.string()
+            .email({ maxDomainSegments: 2 })
+            .required()
+            .messages({
+                "string.base": "Email must be a string!",
+                "string.email": "Please enter a valid email!",
+                "string.empty": "Email field cannot be empty!",
+                "any.required": "Email field is required!",
+            }),
+        // PASSWORD
+        password: Joi.string()
+            .required()
+            .messages({
+                "string.base": "Password must be a string!",
+                "string.empty": "Password field cannot be empty!",
+                "any.required": "Password field is required!",
+            }),
+    }).and("email", "password")
+};
+// RESET PASSWORD SCHEMA 
+export const resetPasswordSchema = {
+    body: Joi.object({
+        // EMAIL
+        email: Joi.string()
+            .email({ maxDomainSegments: 2 })
+            .required()
+            .messages({
+                "string.base": "Email must be a string!",
+                "string.email": "Please enter a valid email!",
+                "string.empty": "Email field cannot be empty!",
+                "any.required": "Email field is required!",
+            }),
+        // NEW PASSWORD
+        newPassword: Joi.string().length(6)
+            .required()
+            .messages({
+                "string.base": "New Passsword must be a string!",
+                "string.empty": "New Passsword field cannot be empty!",
+                "any.required": "New Passsword field is required!",
+                "string.length": "New Passsword length should be 6 characters !",
+            }),
+        // CONFIRM PASSWORD
+        confrimPassword: Joi.string().length(6)
+            .required()
+            .valid(Joi.ref("newPassword"))
+            .messages({
+                "string.base": "Confirm password must be a string!",
+                "string.empty": "Confirm password field cannot be empty!",
+                "any.required": "Confirm password field is required!",
+                "string.length": "Confirm password length should be 6 characters !",
+                "any.only": "Confirm password must match password."
+            })
+    }).and("newPassword", "confrimPassword")
+}
+// SIGN UP SCHEMA 
+export const signupSchema = {
+    body: loginSchema.body.keys({
+        // FIRST NAME 
+        firstName: Joi.string().required().messages({
+            "string.base": "First Name  must be a string!",
+            "string.empty": "First Name  field cannot be empty!",
+            "any.required": "First Name  field is required!",
+        }),
+        // LAST NAME 
+        lastName: Joi.string().required().messages({
+            "string.base": "Last Name  must be a string!",
+            "string.empty": "Last Name  field cannot be empty!",
+            "any.required": "Last Name  field is required!",
+        }),
+        // PHONE NUMBER 
+        phoneNumber: Joi.string().required().length(11).messages({
+            "string.base": "Phone Number  must be a string!",
+            "string.empty": "Phone Number  field cannot be empty!",
+            "any.required": "Phone Number field is required!",
+            "string.length": "Phone Number length should be 11 characters !",
+        }),
+        // AGE
+        age: Joi.number().required().min(18).max(100).messages({
+            "number.base": "Age  must be a string!",
+            "number.empty": "Age  field cannot be empty!",
+            "any.required": "Age  field is required!",
+            "number.max": "Age max length should be 100 characters !",
+            "number.min": "Age min length should be 18 characters !",
+        }),
+        // ADDRESS
+        address: Joi.string().empty("").default("Not data provided !").messages({
+            "string.base": "Address must be a string!",
+        }),
+        // GENDER
+        gender: Joi.string().required().valid(Gender.MALE, Gender.FEMALE).messages({
+            "string.base": "Gender must be a string!",
+            "string.empty": "Gender  field cannot be empty!",
+            "any.required": "Gender  field is required!",
+        }),
+        // ROLE
+        role: Joi.string().empty("").default(Role.USER).valid(Role.USER, Role.ADMIN).messages({
+            "string.base": "Role must be a string!",
+        }),
+    })
+}
